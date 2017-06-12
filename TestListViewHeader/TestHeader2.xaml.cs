44efddcfe0845784f54909d85464a8788a0ef6e4
blue-fire <909683964@qq.com>
2017-06-12 22:44:54 +0800

    FIRST

--- TestListViewHeader/TestHeader2.xaml.cs
+++ TestListViewHeader/TestHeader2.xaml.cs
@@ -43,31 +43,6 @@ namespace TestListViewHeader
             }
         }
 
-        static T FindFirstChild<T>(FrameworkElement element) where T : FrameworkElement
-        {
-            int childrenCount = VisualTreeHelper.GetChildrenCount(element);
-            var children = new FrameworkElement[childrenCount];
-
-            for (int i = 0; i < childrenCount; i++)
-            {
-                var child = VisualTreeHelper.GetChild(element, i) as FrameworkElement;
-                children[i] = child;
-                if (child is T)
-                    return (T)child;
-            }
-
-            for (int i = 0; i < childrenCount; i++)
-                if (children[i] != null)
-                {
-                    var subChild = FindFirstChild<T>(children[i]);
-                    if (subChild != null)
-                        return subChild;
-                }
-
-            return null;
-        }
-
-
         private void _pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             _headerVisual?.StopAnimation("Offset.Y");
@@ -113,6 +88,31 @@ namespace TestListViewHeader
                 {
                     UpdateAnimation();
                 };
+        }
+
+
+        static T FindFirstChild<T>(FrameworkElement element) where T : FrameworkElement
+        {
+            int childrenCount = VisualTreeHelper.GetChildrenCount(element);
+            var children = new FrameworkElement[childrenCount];
+
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i) as FrameworkElement;
+                children[i] = child;
+                if (child is T)
+                    return (T)child;
+            }
+
+            for (int i = 0; i < childrenCount; i++)
+                if (children[i] != null)
+                {
+                    var subChild = FindFirstChild<T>(children[i]);
+                    if (subChild != null)
+                        return subChild;
+                }
+
+            return null;
         }
     }
 }
